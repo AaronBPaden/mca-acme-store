035b9c27721340207ca0a1ed53b8af42385c61a1
Aaron Paden <aaronbpaden@gmail.com>
2022-05-11 14:20:16 -0500

    Implement dynamic header.

--- acme-react-app/src/App.js
+++ acme-react-app/src/App.js
@@ -1,9 +1,17 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { useState, useEffect } from 'react';
 function App(props) {
+  let [ dimensions, setDimensions ] = useState({width: 0, height: 0});
+  useEffect(() => {
+    window.addEventListener('resize', ev => {
+      const { innerWidth: width, innerHeight: height } = window;
+      setDimensions({width, height});
+    });
+  }, []);
   return (
     <>
-      <Header />
+      <Header width={dimensions.width} />
       {props.route}
       <Footer />
     </>
