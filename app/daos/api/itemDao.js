9141e3c2fa2c50c5006f8368de2d8dc6a99440d4
Aaron Paden <aaronbpaden@gmail.com>
2022-05-19 11:55:12 -0500

    Remove old commented code.

--- app/daos/api/itemDao.js
+++ app/daos/api/itemDao.js
@@ -33,59 +33,6 @@ class ItemDao extends DaoPublic {
     if (rows) this._spreadRows(res, rows);
   }
 
-  // create: (req, res) => {
-  //   if (Object.keys(req.body).length === 0) {
-  //     res.json({
-  //       error: true,
-  //       message: "No fields to create."
-  //     });
-  //     return;
-  //   }
-  //   const fields = Object.keys(req.body);
-  //   const values = Object.values(req.body);
-  //   con.execute(
-  //     `INSERT INTO ${dao.table} SET ${fields.join(' = ?, ')} = ?;`,
-  //     values,
-  //     (err, dbres) => {
-  //       if(err) {
-  //         console.log('DAO ERROR', err);
-  //         res.send('ERROR creating record');
-  //         return;
-  //       }
-  //       res.send(`Last id: ${dbres.insertId}`);
-  //     }
-  //   );
-  // },
-  // update: (req, res) => {
-  //   if (isNaN(req.params.id)) {
-  //     res.json({
-  //       error: true,
-  //       message: "ID must be a number"
-  //     });
-  //     return;
-  //   }
-  //   if (Object.keys(req.body).length === 0) {
-  //     res.json({
-  //       error: true,
-  //       message: "No fields to update."
-  //     });
-  //     return;
-  //   }
-  //   const fields = Object.keys(req.body);
-  //   const values = Object.values(req.body);
-  //   con.execute(
-  //     `UPDATE actor SET ${fields.join(' = ?, ')} = ? WHERE actor_id = ?;`,
-  //     [...values, req.params.id],
-  //     (err, dbres) => {
-  //       if(err) {
-  //         console.log('DAO ERROR', err);
-  //         res.send('ERROR updating record');
-  //         return;
-  //       }
-  //       res.send(`changed: ${dbres.changedRows}`);
-  //     }
-  //   );
-  // },
 }
 
 export {
